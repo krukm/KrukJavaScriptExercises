e66038d6b9fbba1a485ff435ad8515828d25ff01
Matthew Kruk <matthew.kruk@gmail.com>
2018-06-06 07:50:59 -0400

    add exercise 4, refactor

--- script.js
+++ script.js
@@ -15,12 +15,12 @@ function do_math(num) {
 
 function add_strings() {
   var strings = "";
-  var answer = prompt("Do you want to play?");
+  var answer = prompt("Do you want to play?", "yes");
     if(answer === 'yes') {
      do {
       var string = prompt("Enter a word.");
       strings += string + " ";
-      answer = prompt("Do you want to play, again?");
+      answer = prompt("Do you want to play, again?", "yes");
     } while (answer != 'no' && answer == 'yes');
     document.getElementById("concat").innerHTML = strings;
     console.log(strings);
@@ -30,7 +30,7 @@ function add_strings() {
 }
 
 function user_name() {
-  var reply = prompt("Would you like to print your name?");
+  var reply = prompt("Would you like to print your name?", "yes");
   var name;
   if(reply === 'yes') {
     name = prompt("Enter your name:");
@@ -41,7 +41,26 @@ function user_name() {
   while(reply === 'yes') {
     name += "!";
     alert("Hello. My name is " + name);
-    reply = prompt("Would you like to print this again?");
+    reply = prompt("Would you like to print this again?", "yes");
   }
   document.getElementById("excited_name").innerHTML = "Hello. My name is " + name;
+}
+
+function meal_time() {
+  var time_of_day = prompt("What time of day is it? morning, noon, or evening");
+
+  switch(time_of_day) {
+    case "morning":
+      document.getElementById("time_of_day").innerHTML = "Since it is morning, you should be eating breakfast. We suggest eggs and toast.";
+      break;
+    case "noon":
+      document.getElementById("time_of_day").innerHTML = "Since it is noon, you should be eating lunch. We suggest a salad.";
+      break;
+    case "evening":
+      document.getElementById("time_of_day").innerHTML = "Since it is evening, you should be eating dinner. We suggest chicken and rice.";
+      break;
+    default:
+      alert("Please enter a valid time of day: morning, noon, or evening.");
+      time_of_day = prompt("What time of day is it? morning, noon, or evening");
+  }
 }
